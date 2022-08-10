2fcbcde485d51fcae1f511d8395fcfef5fe70e7a
Max Gorbachev <gorbachyov.maksim@mail.ru>
2022-08-10 13:55:15 +0300

    Initial Commit

---
+++ VKApp_MG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKApp_MG
+//
+//  Created by Максим on 10.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
